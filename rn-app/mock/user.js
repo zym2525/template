f0f2bf3153bba0572c2f0c0a0c102d92dc0b49f0
zym2525 <540361168@qq.com>
2021-01-19 14:50:23 +0800

    change mock method

---
+++ rn-app/mock/user.js
@@ -0,0 +1,45 @@
+
+const Mock = require('mockjs')
+const { ajaxResponse } = require('./utils')
+
+module.exports = [
+    // user login
+    {
+        url: '/api/Account/authenticate',
+        type: 'post',
+        response: config => {
+
+            return {
+                code: 1,
+                data: ajaxResponse(Mock.mock({
+                    'items|3': [{
+                        id: '@id',
+                        title: '@sentence(10, 20)',
+                        'status|1': ['published', 'draft', 'deleted'],
+                        author: 'name',
+                        display_time: '@datetime',
+                        pageviews: '@integer(300, 5000)'
+                    }]
+                }))
+            }
+        }
+    },
+    {
+        url: '/api/getInfo',
+        type: 'get',
+        response: config => {
+            return ajaxResponse({
+                data: Mock.mock({
+                    'items|10': [{
+                        id: '@id',
+                        title: '@sentence(10, 20)',
+                        'status|1': ['published', 'draft', 'deleted'],
+                        author: 'name',
+                        display_time: '@datetime',
+                        pageviews: '@integer(300, 5000)'
+                    }]
+                })
+            })
+        }
+    },
+]
