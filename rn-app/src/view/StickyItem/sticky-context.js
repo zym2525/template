dccb581644099215d06370ea2608fbd767a3b511
zym2525 <540361168@qq.com>
2021-06-15 13:10:22 +0800

    change

--- rn-app/src/view/StickyItem/sticky-context.js
+++ rn-app/src/view/StickyItem/sticky-context.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 export const StickyContext = React.createContext({
     scrollY: new Animated.Value(0)
